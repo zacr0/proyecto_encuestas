fc4b727dd082b5d178ae678d72c6002c94b47c3c
zacr0 <zacro.suarez@gmail.com>
2014-01-28 09:26:07 +0100

    Cambios hechos en Conexion

--- clases_bbdd.php
+++ clases_bbdd.php
@@ -2,6 +2,18 @@
 
 class Conexion
 {
-	//hola mariconsones
+	var $con;
+
+	function Conexion($server, $user, $pass, $bd)
+	{
+		//Datos del servidor de BBDD	$connection['server'] = $server;
+		$connection['user'] = $user;
+		$connection['pass'] = $pass;
+		$connection['base'] = $bd;
+
+		// Creacion de la conexion con los datos recibidos
+		$connect = mysqli_connect($connection['server'], $connection['user'], $connection['pass'], $connection['base']);
+
+	}
 }
 ?>
