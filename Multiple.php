4b94fe9a1d8a19de9f8531a846756897822d2555
zacr0 <zacro.suarez@gmail.com>
2014-01-31 09:56:49 +0100

    Modificado multiple

--- Multiple.php
+++ Multiple.php
@@ -1,69 +1,71 @@
 <?php
-class Multiple{
-	
+class Multiple {
+
 	private $id;
 	private $opcion;
 	private $idpregunta;
-	
-	function Multiple(){
-		
-		$multiple=new Consulta();
-		$consulta="SELECT * FROM  `opcionesrespuestamultiple` ";
-		$resultado=$multiple->ejecutarConsulta($consulta);
-		$row=mysqli_fetch_array($resultado);
-		
-		$this-> id=$row["idOpcionesRespuestaMultiple"];
-		$this-> opcion=$row["opcion"];
-		$this-> idpregunta=$row["Preguntas_idPreguntas"];
-		
-		
+
+	function Multiple() {
+
+		$multiple = new Consulta();
+		$consulta = "SELECT * FROM  `opcionesrespuestamultiple` ";
+		$resultado = $multiple -> ejecutarConsulta($consulta);
+		$row = mysqli_fetch_array($resultado);
+
+		$this -> id = $row["idOpcionesRespuestaMultiple"];
+		$this -> opcion = $row["opcion"];
+		$this -> idpregunta = $row["Preguntas_idPreguntas"];
+
 	}
-	
+
 	//Metodos get
-	
-	function getId(){
-		return $this->id;
+
+	function getId() {
+		return $this -> id;
 	}
-	function getOpcion(){
-		return $this->opcion;
+
+	function getOpcion() {
+		return $this -> opcion;
 	}
-	function getIdpregunta(){
-		return $this->idpregunta;
+
+	function getIdpregunta() {
+		return $this -> idpregunta;
 	}
+
 	//Metodos set
-	
-	function setOpcion($v){
-		$this->id=$v;
+
+	function setOpcion($v) {
+		$this -> id = $v;
 	}
-	
-	
-	function guardar(){
-		if($this->id){
-			$this->actualizarMultiple();
-		}else{
-			$this->insertarMultiple();
+
+	function guardar() {
+		if ($this -> id) {
+			$this -> actualizarMultiple();
+		} else {
+			$this -> insertarMultiple();
 		}
 	}
-	
-	function actualizarMultiple(){
-		$obj_multiple=new Consulta();
-		$consulta="UPDATE  `encuestas`.`opcionesrespuestamultiple` SET  `opcion` =  '$this->opcion' WHERE  `opcionesrespuestamultiple`.`idOpcionesRespuestaMultiple` =$this->id AND  `opcionesrespuestamultiple`.`Preguntas_idPreguntas` =$this->idpregunta;";
-		$obj_multiple->ejecutarConsulta($consulta);
-		return $obj_multiple->getAfectados();
-	}  
-	function insertarMultiple(){
-		$obj_multiple=new Consulta();
-		$consulta="INSERT INTO  `encuestas`.`opcionesrespuestamultiple` (`idOpcionesRespuestaMultiple` ,`opcion` ,`Preguntas_idPreguntas`)VALUES (NULL ,  '$this->opcion',  '$this->idpregunta');";
-		$obj_multiple->ejecutarConsulta($consulta);
-		return $obj_multiple->getAfectados();
-	}  
-	function eliminar(){
-		$obj_multiple=new Consulta();
-		$consulta="DELETE FROM `encuestas`.`opcionesrespuestamultiple` WHERE `opcionesrespuestamultiple`.`idOpcionesRespuestaMultiple` = $this->id AND `opcionesrespuestamultiple`.`Preguntas_idPreguntas` = $this->idpregunta";
-		$obj_multiple->ejecutarConsula($consulta);
-		return $obj_multiple->getAfectados();
-	}           
+
+	function actualizarMultiple() {
+		$obj_multiple = new Consulta();
+		$consulta = "UPDATE  `encuestas`.`opcionesrespuestamultiple` SET  `opcion` =  '$this->opcion' WHERE  `opcionesrespuestamultiple`.`idOpcionesRespuestaMultiple` =$this->id AND  `opcionesrespuestamultiple`.`Preguntas_idPreguntas` =$this->idpregunta;";
+		$obj_multiple -> ejecutarConsulta($consulta);
+		return $obj_multiple -> getAfectados();
+	}
+
+	function insertarMultiple() {
+		$obj_multiple = new Consulta();
+		$consulta = "INSERT INTO  `encuestas`.`opcionesrespuestamultiple` (`idOpcionesRespuestaMultiple` ,`opcion` ,`Preguntas_idPreguntas`)VALUES (NULL ,  '$this->opcion',  '$this->idpregunta');";
+		$obj_multiple -> ejecutarConsulta($consulta);
+		return $obj_multiple -> getAfectados();
+	}
+
+	function eliminar() {
+		$obj_multiple = new Consulta();
+		$consulta = "DELETE FROM `encuestas`.`opcionesrespuestamultiple` WHERE `opcionesrespuestamultiple`.`idOpcionesRespuestaMultiple` = $this->id AND `opcionesrespuestamultiple`.`Preguntas_idPreguntas` = $this->idpregunta";
+		$obj_multiple -> ejecutarConsula($consulta);
+		return $obj_multiple -> getAfectados();
+	}
+
 }
-
-
 ?>
