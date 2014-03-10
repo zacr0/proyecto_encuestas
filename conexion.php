501dbfb42fa89cc0d1c53e21b3540875ea6ec0b8
zacr0 <zacro.suarez@gmail.com>
2014-03-10 18:08:15 +0100

    Tramas

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php 
+// datos para la coneccion a mysql
+define('DB_SERVER','localhost');
+define('DB_NAME','yolo');
+define('DB_USER','root');
+define('DB_PASS','root');
+ 
+$conex = mysql_connect (DB_SERVER,DB_USER,DB_PASS);
+mysql_select_db(DB_NAME,$conex);
