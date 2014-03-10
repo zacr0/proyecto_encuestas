7d4d5bcb31d12b03a958aae4c8c0d6a768fcd3ac
zacr0 <zacro.suarez@gmail.com>
2014-03-10 18:27:51 +0100

    Cambios en conexion

--- conexion.php
+++ conexion.php
@@ -1,9 +1,8 @@
-<?php 
+<?php
 // datos para la coneccion a mysql
-define('DB_SERVER','localhost');
-define('DB_NAME','yolo');
-define('DB_USER','root');
-define('DB_PASS','root');
- 
-$conex = mysql_connect (DB_SERVER,DB_USER,DB_PASS);
-mysql_select_db(DB_NAME,$conex);
+define('DB_SERVER', 'localhost');
+define('DB_NAME', 'encuestas');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+
+$conex = mysql_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME) or die("Error en la conexión con la base de datos");
